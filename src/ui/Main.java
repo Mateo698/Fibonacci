3303b09eea2f12dff6fcee98f10c42c7c197102f
Mateo <mateora73@gmail.com>
2021-08-10 16:54:41 -0500

    First

---
+++ src/ui/Main.java
@@ -0,0 +1,24 @@
+package ui;
+
+import java.awt.desktop.ScreenSleepEvent;
+
+public class Main {
+
+	public long iterative(long quantity) {
+		
+		return 0;
+	}
+	
+	public long recursive(long quantity) {
+		return 0;
+	}
+	
+	public static void main(String[] args) throws InterruptedException {
+		long enterp = 900;
+		for (int i = 0; i < 76; i++) {
+			enterp++;
+			System.out.println(enterp);
+			Thread.sleep(100);
+		}
+	}
+}
